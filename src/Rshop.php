e9d1a8daf838747d6622ba9eb5887d21c7365672
Tomas Saghy <tomas.saghy@gmail.com>
2024-09-18 15:41:48 +0200

    add null_adjustment - always_last

--- src/Rshop.php
+++ src/Rshop.php
@@ -41,6 +41,9 @@ class Rshop extends Config
         'ordered_class_elements' => [
             'order' => ['use_trait', 'case', 'constant_public', 'constant_protected', 'constant_private', 'property_public', 'property_protected', 'property_private', 'construct', 'destruct', 'method_public', 'method_protected', 'method_private', 'magic', 'phpunit']
         ],
+        'ordered_types' => [
+            'null_adjustment' => 'always_last'
+        ],
         'phpdoc_order' => true,
         'phpdoc_types_order' => [
             'null_adjustment' => 'always_last',
